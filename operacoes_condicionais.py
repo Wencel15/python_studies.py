a783179a4963566c5ce769378b2aa427ab595751
Luciano Wencel <90798078+Wencel15@users.noreply.github.com>
2023-10-30 16:21:28 -0300

    Create operacoes_condicionais.py

---
+++ operacoes_condicionais.py
@@ -0,0 +1,33 @@
+# if / elif / else
+#se / se não se / se não
+
+entrada = input('Você quer "entrar" ou "sair"? ')
+
+if entrada == 'entrar':
+    print('Você entrou no sistema')
+elif entrada == 'sair':
+    print('Você saiu do sistema')
+else:
+    print('Você não digitou nem entrar e nem sair.')
+
+#Exemplo 2
+
+condicao1 = False
+condicao2 = False
+condicao3 = False
+condicao4 = True
+
+
+if condicao1:
+    print('Código para condição 1')
+    print('Código para condição 1')
+elif condicao2:
+    print('Código para condição 2')
+elif condicao3:
+    print('Código para condição 3')
+elif condicao4:
+    print('Código para condição 4')
+else:
+    print('Nenhuma condição foi satisfeita.')
+
+print('Fora do if')
