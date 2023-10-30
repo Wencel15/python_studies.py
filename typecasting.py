d50f5008de2bf3777a3eaf98ab9c0b81107c5f74
Luciano Wencel <90798078+Wencel15@users.noreply.github.com>
2023-10-30 14:37:35 -0300

    Create typecasting.py

---
+++ typecasting.py
@@ -0,0 +1,21 @@
+"""
+Conversão de tipos, coerção
+type convertion, typecasting, coercion
+é o ato de converter um tipo em outro
+tipos imutaveis e primitívos:
+str, int, float, bool
+"""
+
+# Classe int
+print('1', type('1'))
+print(int('1'), type(int('1')))
+print(int('1') + 1)
+
+# Classe float
+
+print(float('1') + 1)
+
+# Classe bool
+
+print(bool(''))
+print(bool('1'))
