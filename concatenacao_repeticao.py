a323e8920c4edf010dafe1eb15a3caff8dec73b1
Luciano Wencel <90798078+Wencel15@users.noreply.github.com>
2023-10-30 15:16:25 -0300

    Create concatenacao_repeticao.py

---
+++ concatenacao_repeticao.py
@@ -0,0 +1,11 @@
+#Concatenação
+
+concatenacao = 'A' + 'B' + ' ' + 'C'
+print(concatenacao)
+
+#Repetição
+
+a_dez_vezes = 'A' * 10
+tres_vezes_wencel = 3 * 'Wencel'
+print(a_dez_vezes)
+print(tres_vezes_wencel)
