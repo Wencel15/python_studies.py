d4b1e4e8e30a37b5dc6f4feff648bd1427a99ee3
Luciano Wencel <90798078+Wencel15@users.noreply.github.com>
2023-10-30 14:52:53 -0300

    Create variaveis.py

---
+++ variaveis.py
@@ -0,0 +1,22 @@
+"""
+Variáveis são usadas para salvar algo na memória do computador.
+PEP8: inicie variáveis com letras minpusculas,
+pode usar números e underline_.
+O sinal de = é o operador de atribuição. Ele é usado para 
+atribuir um valor a um nome (variável).
+Uso: nome_variavel = expressão
+"""
+
+nome_completo = 'Luciano Wencel'
+soma_dois_mais_dois = 2 + 2
+inteiro_um = int('1')
+
+print(inteiro_um, type(inteiro_um))
+print(nome_completo, soma_dois_mais_dois)
+
+
+nome = 'Wencel'
+idade = 34
+maior_de_idade = idade >= 18
+print('Nome:', nome, 'Idade:', idade,)
+print('E maior de idade?', maior_de_idade)
