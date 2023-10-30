5bfdc54fb0789ae62d5e507c48f9b4d57a8c026d
Luciano Wencel <90798078+Wencel15@users.noreply.github.com>
2023-10-30 13:50:34 -0300

    Create funcao_print.py

---
+++ funcao_print.py
@@ -0,0 +1,17 @@
+# função print imprime os argumentos correspondetes  
+# \r\n faz com que o computador quebre a linha autolaticamente = CRLF
+
+print(12) #Imprime 1 Argumento
+
+print(12, 34) #Imprime 2 ou mais argumentos utilizando ,
+
+#Separador utilizado para separar os argumentos
+
+print(12, 34, 1011, sep='Sep') 
+
+#Utilizando o end=
+# podemos retirar a ou colocar a quebra de linha
+
+print(12, 34, 1011, sep='Sep', end='##')
+print(12, 34, 1011, sep='Sep', end='\n##')  #\n para quebras de linha, antes ou depois
+print(1,2,3) #exemplificando o end.
