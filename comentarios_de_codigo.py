c905a70a2231116fff6272431bfe8c92154a60f1
Luciano Wencel <90798078+Wencel15@users.noreply.github.com>
2023-10-30 13:36:18 -0300

    Create comentarios_de_codigo.py

---
+++ comentarios_de_codigo.py
@@ -0,0 +1,19 @@
+# Permite escrever um comentário
+print(123) # Na frente
+#Abaixo
+print(123)
+
+#Criando comentarios multilinhas com DocString:
+
+"""DocString"""
+
+"""
+E escrever o que 
+eu quiser
+pulando linhas
+"""
+print(456)
+'''
+Usar aspas simples
+ou aspas duplas
+'''
