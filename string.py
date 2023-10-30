49f0a3cc1b2f6ab4da84996096ae9867fa895ae2
Luciano Wencel <90798078+Wencel15@users.noreply.github.com>
2023-10-30 14:04:04 -0300

    Create string.py

---
+++ string.py
@@ -0,0 +1,19 @@
+"""
+Python = Linguagem de programação
+Tipo de tipagem = Dinamica / forte
+str -> string -> texto
+Strings são textos que estão dentro de aspas
+"""
+
+# Aspas simples
+print('Luciano Wencel')
+print(1, 'Luciano "Wencel"') #para aparecer as aspas só inserir aspas duplas e aspas simples 
+
+#Aspas duplas
+print("Luciano Wencel")
+
+#Escape com barra invertida \
+print("Luciano \"Wencel")
+
+#r mostra os caracteres de escape
+print(r"Luciano \"Wencel\"")
