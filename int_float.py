a04d61415217a196fc05f433a30c0c1d5218e5b8
Luciano Wencel <90798078+Wencel15@users.noreply.github.com>
2023-10-30 14:17:39 -0300

    Create int_float.py

---
+++ int_float.py
@@ -0,0 +1,27 @@
+#Tipos int e float
+
+"""
+int -> Número inteiro
+O tipo int representa qualquer número
+positivo ou negativo. 
+int sem sinal é considerado positivo
+"""
+print(11) # int
+print(-11) # int
+
+"""
+float -> Número com ponto flutuante
+O tipo float representa qualquer número
+positivo ou negativo com ponto flutuante.
+float sem sinalé considerado positivo
+"""
+print(1.1) #float
+print(-1.1) #float
+
+#A função(clase) type() mostra o tipo que o Python inferiu ao valor.
+
+print(type('Wencel'))
+print(type('1'))
+print(type(1))
+print(type(1.1))
+print(type(1.1), type('Wencel'),type(1))
