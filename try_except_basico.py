26870d66cec829875cdfb79ef1210b290a229013
Luciano Wencel <90798078+Wencel15@users.noreply.github.com>
2023-11-02 13:14:32 -0300

    Create try_except_basico.py

---
+++ try_except_basico.py
@@ -0,0 +1,19 @@
+"""
+Introdução ao try/except
+try -> tentar executar o código
+except -> ocorreu algum erro ao tentar executar
+"""
+numero_str = input('Vou dobrar o npumero que você digitar: ')
+
+if numero_str.isdigi():
+    numero_float = float(numero_str)
+    print(f'O dobro de {numer_str} é {numero_float * 2:.2f}')
+else:
+    print('Isso não é um número')
+
+try:
+    numero_float = float(numero_str)
+    print('FLOAT:', numero_float)
+    print(f'O dobro de {numer_str} é {numero_float * 2:.2f}')
+except:
+    print('Isso não é um número')
