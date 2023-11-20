e5062ba3317d2a0d637721a42e8da5a1b4dcf439
Luciano Wencel <90798078+Wencel15@users.noreply.github.com>
2023-11-20 20:08:37 -0300

    Update while.py

--- while.py
+++ while.py
@@ -51,3 +51,21 @@ while contador <= 100:
         break
 
 print('Acabou')
+
+"""
+While + While
+"""
+qtd_linhas = 5
+qtd_colunas = 5
+
+linha = 1
+while linha <= qtd_linhas:
+    coluna = 1
+    
+    while coluna <= qtd_colunas:
+        print(f'{linha=}, {coluna=}')
+        coluna += 1
+
+    linha += 1
+
+print('Acabou')
