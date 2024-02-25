1a5a08225f81af407f5d954b99df817aea48e553
Luciano Wencel <90798078+Wencel15@users.noreply.github.com>
2024-02-25 14:03:34 -0300

    Create for_range.py

---
+++ for_range.py
@@ -0,0 +1,14 @@
+"""
+For + Range
+range -> range(start, stop, step)
+"""
+#numeros = range(10)
+#numeros = range(5, 10)
+#numeros = range(5, 10 ,2)
+#print(numeros)
+#print(numeros[5])
+
+numeros = range(0, 10, 2)
+
+for numero in numeros:
+    print(numero)
