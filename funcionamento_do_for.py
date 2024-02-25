5e0d5b160e82370c249b1db97b05fa6862870578
Luciano Wencel <90798078+Wencel15@users.noreply.github.com>
2024-02-25 14:31:30 -0300

    Update funcionamento_do_for.py

--- funcionamento_do_for.py
+++ funcionamento_do_for.py
@@ -43,3 +43,22 @@ while True:
         print(letra)
     except StopIteration:
         break
+
+#3
+
+for i in range(10):
+    if i == 2:
+        print('i é 2, pulando...')
+        continue
+
+    if i == 8:
+        print('i é 8, seu else não executará')
+        break
+
+    for j in range(1, 3):
+        print(i, j)
+
+else:
+    print('For completo com sucesso!')
+
+
